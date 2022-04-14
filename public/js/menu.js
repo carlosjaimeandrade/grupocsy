59d4fc938696d22fada719b13a9e8b49f3574cf0
Antônio Edinadson Gomes Eugênio <antonioedinadson@gmail.com>
2022-04-13 22:53:26 -0300

    create effects menu

--- public/js/menu.js
+++ public/js/menu.js
@@ -1,8 +1,15 @@
-const open = document.querySelector('.menuHamburger i');
-
-function toggleMobile(e) {
-    const menu = document.querySelector('.menuItems');                  
+function toggleMobile(div) {
+    const menu = document.querySelector(div);                  
     menu.classList.toggle('active');        
 }
 
-open.addEventListener('click', toggleMobile);
+const open = document.querySelector('.menuHamburger i');
+const showEmpresas = document.querySelector('.menuListItem');
+
+open.addEventListener('click', () => toggleMobile('.menuItems'));
+
+showEmpresas.addEventListener('click', (e) => e.preventDefault());
+showEmpresas.addEventListener('mouseenter', () => toggleMobile('.menuListItem .list'));
+showEmpresas.addEventListener('mouseleave', () => toggleMobile('.menuListItem .list'));
+
+
