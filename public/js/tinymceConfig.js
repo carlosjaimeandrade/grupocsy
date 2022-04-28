f6f25fcf56bbbc17af4ac672a236656835909016
Carlos Jaime <jaime_andrek@hotmail.com>
2022-04-28 12:24:27 -0300

    incorporando o Tiny na pagina de publicação

---
+++ public/js/tinymceConfig.js
@@ -0,0 +1,18 @@
+tinymce.init({
+    language: 'pt_BR',
+    selector: "#post",
+    plugins: [
+        'advlist autolink link image lists print hr searchreplace wordcout fullscreen insertdatetime media save table paste emoticons'
+    ]
+})
+
+function newPost() {
+    let new_post_container = document.querySelector('[new-post]')
+    new_post_container.style.display = new_post_container.style.display == 'flex' ? 'none' : 'flex'
+}
+
+const new_post_close = document.querySelector('[close]')
+let new_post_container = document.querySelector('[new-post]')
+new_post_close.onclick = () => {
+    new_post_container.style.display = 'none'
+}
