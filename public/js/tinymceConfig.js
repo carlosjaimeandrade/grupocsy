7d4bebdde81fc6df21306d686d0bab6c9fe7e26a
Carlos Jaime <jaime_andrek@hotmail.com>
2022-04-28 21:02:40 -0300

    modal de publicação

--- public/js/tinymceConfig.js
+++ public/js/tinymceConfig.js
@@ -3,13 +3,15 @@ tinymce.init({
     selector: "#post",
     plugins: [
         'advlist autolink link image lists print hr searchreplace wordcout fullscreen insertdatetime media save table paste emoticons'
-    ]
+    ],
+    height: "290"
 })
 
 function newPost() {
     let new_post_container = document.querySelector('[new-post]')
     new_post_container.style.display = new_post_container.style.display == 'flex' ? 'none' : 'flex'
 }
+
 
 const new_post_close = document.querySelector('[close]')
 let new_post_container = document.querySelector('[new-post]')
