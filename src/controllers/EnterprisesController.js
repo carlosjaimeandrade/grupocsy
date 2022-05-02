799e3e4b0a88a41f1e551d398e35a230ba4c5a80
Carlos Jaime <jaime_andrek@hotmail.com>
2022-05-02 20:17:39 -0300

    Listando projetos futuros e empreendimento

--- src/controllers/EnterprisesController.js
+++ src/controllers/EnterprisesController.js
@@ -1,5 +1,25 @@
-const enterprises = (req, res) => {
-    res.render('pages/enterprises')
+const Publication = require('../models/Publication')
+const Sequelize = require('sequelize')
+
+const enterprises = async(req, res) => {
+    const publications = await Publication.findAll({
+        attributes: {
+            include: [
+                [Sequelize.fn('date_format', Sequelize.col('createdAt'), "%d-%m-%Y"), 'd'],
+                [Sequelize.fn('date_format', Sequelize.col('createdAt'), "%H-%i-%s"), 't'],
+            ]
+        },
+        where: {
+            category: 'empreendimentos'
+        },
+        raw: true
+
+    })
+
+
+    res.render('pages/enterprises', {
+        publications: publications
+    })
 }
 
 module.exports = {
--- src/controllers/FutureProjectsController.js
+++ src/controllers/FutureProjectsController.js
@@ -1,5 +1,24 @@
-const futureProjects = (req, res) => {
-    res.render('pages/futureProjects')
+const Publication = require('../models/Publication')
+const Sequelize = require('sequelize')
+
+const futureProjects = async(req, res) => {
+    const publications = await Publication.findAll({
+        attributes: {
+            include: [
+                [Sequelize.fn('date_format', Sequelize.col('createdAt'), "%d-%m-%Y"), 'd'],
+                [Sequelize.fn('date_format', Sequelize.col('createdAt'), "%H-%i-%s"), 't'],
+            ]
+        },
+        where: {
+            category: 'projetos-futuros'
+        },
+        raw: true
+
+    })
+
+    res.render('pages/futureProjects', {
+        publications: publications
+    })
 }
 
 module.exports = {
