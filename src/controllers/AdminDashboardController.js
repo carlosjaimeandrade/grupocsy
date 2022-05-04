85f7d7735cc026eb5e47c4215cb3c25c9ced8157
Antônio Edinadson Gomes Eugênio <antonioedinadson@gmail.com>
2022-05-04 00:28:25 -0300

    corrigindo resposvidade admin panel

--- src/controllers/AdminDashboardController.js
+++ src/controllers/AdminDashboardController.js
@@ -1,19 +1,22 @@
 const Users = require('../Models/User');
+const Publication = require('../Models/Publication');
 
 module.exports = {
     showDashboard: async (req, res) => {
 
         const users = await Users.findAll({ limit: 3, order: [['id', 'DESC']], raw: true });
+        const publications = await Publication.findAll();
 
         if (!users) {
             users = null;
             return;
         }
-    
+
         res.render('pages/admin/home', {
             message: req.flash('message'),
             type: req.flash('type'),
             users,
+            publications: { publications: publications.slice(0, 2), count: publications.length },
         })
     },
 }
