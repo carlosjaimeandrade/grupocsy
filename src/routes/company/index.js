c54a4585af6a8ae663c76e55230ae140702c69a3
Carlos Jaime <jaime_andrek@hotmail.com>
2022-05-19 19:36:31 -0300

    finalizando envio de email na home e nas empresas do grupo

--- src/routes/company/index.js
+++ src/routes/company/index.js
@@ -6,4 +6,6 @@ const CompanyController = require('../../controllers/CompanyController');
 
 routes.get('/empresas/grupocsy', CompanyController.ShowCompany)
 
+routes.post('/empresas/grupocsy', CompanyController.info)
+
 module.exports = routes;
