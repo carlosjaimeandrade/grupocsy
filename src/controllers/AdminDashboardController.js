27d530f8ee3852525d1a231b2995e2d7a04f304c
Carlos Jaime <jaime_andrek@hotmail.com>
2022-05-23 21:51:47 -0300

    Total de faturamento e total de projetos

--- src/controllers/AdminDashboardController.js
+++ src/controllers/AdminDashboardController.js
@@ -1,22 +1,31 @@
+const Sequelize = require('sequelize');
 const Users = require('../Models/User');
 const Publication = require('../Models/Publication');
+const Financial = require('../Models/Financial');
 
 module.exports = {
     showDashboard: async (req, res) => {
 
         const users = await Users.findAll({ limit: 3, order: [['id', 'DESC']], raw: true });
         const publications = await Publication.findAll();
+        const financials = await Financial.findAll({ attributes: [ [Sequelize.fn('sum', Sequelize.col('value')), 'total']],where: { status: "approved" }, raw: true})
+        const projetos = await Publication.findAll({ where: { category: "projetos-futuros" }});
 
         if (!users) {
             users = null;
             return;
         }
 
+       console.log(financials)
+
         res.render('pages/admin/home', {
             message: req.flash('message'),
             type: req.flash('type'),
             users,
             publications: { data: publications.slice(0, 2), count: publications.length },
+            financials,
+            projetos
+            
         })
     },
 }
